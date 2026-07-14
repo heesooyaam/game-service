3bb4d28f49df2b1cb9dc0207c50a7d223fe1d11c
qwerty1337-senior <maximpro172007@gmail.com>
2026-07-14 00:28:29 +0000

    json value task 11

---
+++ library/json/include/json/json_value.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <memory>
+#include <variant>
+#include <vector>
+#include <string>
+#include <unordered_map>
+
+namespace json::value {
+
+    class TJsonValue;
+
+    struct TNull{};
+    using TNumber = double;
+    using TBoolean = bool;
+    using TString = std::string;
+    using TArray = std::vector<TJsonValue>;
+    using TObject = std::unordered_map<TString, TJsonValue>;
+
+    using TArrayPtr = std::unique_ptr<TArray>;
+    using TObjectPtr = std::unique_ptr<TObject>;
+
+    using TStorage = std::variant<TNull, TNumber, TBoolean, TString, TObjectPtr, TArrayPtr, std::monostate>;
+
+
+    class TJsonValue {
+    public:
+
+        TJsonValue() = default;
+
+        explicit TJsonValue(TNull);
+        explicit TJsonValue(TNumber);
+        explicit TJsonValue(TBoolean);
+        explicit TJsonValue(TString&&);
+        explicit TJsonValue(TArray&&);
+        explicit TJsonValue(TObject&&);
+        explicit TJsonValue(const TString&);
+        explicit TJsonValue(const TArray&);
+        explicit TJsonValue(const TObject&);
+
+        explicit TJsonValue(const char*);
+
+        TJsonValue& operator=(TNull);
+        TJsonValue& operator=(TNumber);
+        TJsonValue& operator=(TBoolean);
+        TJsonValue& operator=(TString&&);
+        TJsonValue& operator=(TArray&&);
+        TJsonValue& operator=(TObject&&);
+        TJsonValue& operator=(const TString&);       
+        TJsonValue& operator=(const TArray&);
+        TJsonValue& operator=(const TObject&);
+
+        explicit TJsonValue(const TJsonValue&);
+        explicit TJsonValue(TJsonValue&&) noexcept;
+
+        TJsonValue& operator=(const TJsonValue&);
+        TJsonValue& operator=(TJsonValue&&) noexcept;
+
+        ~TJsonValue() = default;
+    
+    private:
+        TStorage root_value_ = std::monostate();
+
+        static TJsonValue deep_copy(const TJsonValue&);
+    };
+
+} //namespace json::value  
--- library/json/src/json_value.cpp
+++ library/json/src/json_value.cpp
@@ -1,5 +1,143 @@
-#include <game/json/json_value.h>
+#include <json/json_value.h>
 
-namespace game::json {
+namespace {
+    template<class... Ts> struct overload : Ts... { using Ts::operator()...; };
+    template<class... Ts> overload(Ts...) -> overload<Ts...>;
+}
 
-} // namespace game::json
+namespace json::value {
+
+    TJsonValue::TJsonValue(TNull null) 
+        : root_value_(null)
+    {}
+
+    TJsonValue::TJsonValue(TNumber number)
+        : root_value_(number)
+    {}
+
+    TJsonValue::TJsonValue(TBoolean boolean) 
+        : root_value_(boolean)
+    {}
+
+    TJsonValue::TJsonValue(TString&& string) 
+        : root_value_(std::move(string))
+    {}
+
+    TJsonValue::TJsonValue(TArray&& array) 
+        : root_value_(std::make_unique<TArray>(std::move(array)))
+    {}
+
+    TJsonValue::TJsonValue(TObject&& object)
+        : root_value_(std::make_unique<TObject>(std::move(object)))
+    {}
+
+    TJsonValue::TJsonValue(const TString& string)
+        : root_value_(string)
+    {}
+
+    TJsonValue::TJsonValue(const TArray& array) 
+        : root_value_(std::make_unique<TArray>(array))
+    {}
+
+    TJsonValue::TJsonValue(const TObject& object)
+        : root_value_(std::make_unique<TObject>(object))
+    {}
+
+    TJsonValue::TJsonValue(const char* cstr)
+        : root_value_(TString(cstr)) 
+    {}
+    
+    TJsonValue& TJsonValue::operator=(TNull null) {
+        root_value_ = null;
+        return *this;
+    }
+
+    TJsonValue& TJsonValue::operator=(TNumber number) {
+        root_value_ = number;
+        return *this;
+    }
+
+    TJsonValue& TJsonValue::operator=(TBoolean boolean) {
+        root_value_ = boolean;
+        return *this;
+    }
+
+    TJsonValue& TJsonValue::operator=(TString&& string) {
+        root_value_ = std::move(string);
+        return *this;
+    }
+
+    TJsonValue& TJsonValue::operator=(TArray&& array) {
+        root_value_ = std::make_unique<TArray>(std::move(array));
+        return *this;
+    }
+
+    TJsonValue& TJsonValue::operator=(TObject&& object) {
+        root_value_ = std::make_unique<TObject>(std::move(object));
+        return *this;
+    }
+
+    TJsonValue& TJsonValue::operator=(const TString& string) {
+        root_value_ = string;
+        return *this;
+    }     
+
+    TJsonValue& TJsonValue::operator=(const TArray& array) {
+        root_value_ = std::make_unique<TArray>(array);
+        return *this;
+    }
+
+    TJsonValue& TJsonValue::operator=(const TObject& object) {
+        root_value_ = std::make_unique<TObject>(object);
+        return *this;
+    }
+
+    TJsonValue::TJsonValue(const TJsonValue& other) 
+        : root_value_(std::move(deep_copy(other).root_value_))
+    {}
+
+    TJsonValue::TJsonValue(TJsonValue&& other) noexcept 
+        : root_value_(std::move(other.root_value_))
+    {}
+
+    TJsonValue& TJsonValue::operator=(const TJsonValue& other) {
+        if (this == std::addressof(other)) {
+            return *this;
+        }
+        
+        root_value_ = std::move(deep_copy(other).root_value_);
+        return *this;
+    }
+
+    TJsonValue& TJsonValue::operator=(TJsonValue&& other) noexcept {
+        if (this == std::addressof(other)) {
+            return *this;
+        }
+
+        root_value_ = std::move(other.root_value_);
+        return *this;
+    }
+
+    TJsonValue TJsonValue::deep_copy(const TJsonValue& other) {
+        return std::visit(overload{
+            [](const std::monostate&) { return TJsonValue(); },
+            [](const TNull& null) { return TJsonValue(null); },
+            [](const TNumber& number) { return TJsonValue(number); },
+            [](const TBoolean& boolean) { return TJsonValue(boolean); },
+            [](const TString& string) { return TJsonValue(string); },
+            [](const TArrayPtr& array_ptr) { 
+                if (!array_ptr) {
+                    return TJsonValue(TArray());
+                }
+                return TJsonValue(*array_ptr); 
+            },
+            [](const TObjectPtr& object_ptr) { 
+                if (!object_ptr) {
+                    return TJsonValue(TObject());
+                }
+                return TJsonValue(*object_ptr); 
+            }
+        }, other.root_value_);
+    }
+
+} //namespace json::value
